d9c62692e1d91c6e02fec0e938463dfba390093c
Zeinab mansour <zeinabmansour06@gmail.com>
2026-04-02 12:14:23 +0100

    unifying viusuals

--- creerCompte.php
+++ creerCompte.php
@@ -69,33 +69,53 @@
 </head>
 <body>
      <div class="container-fluid p-0">
-        <nav id="mainNav">
-            <div class="logo">KITAB<span class="text_arb">كتاب </span></div>
-                <ul class="nav-links">
-                    <li>
-                        <a href="marketPlace.html"><i class="bi bi-shop fs-6"></i>Marketplace</a>
-                    </li>
-                    <li>
-                        <a href="messages.html"><i class="bi bi-chat-left-text"></i>Messages</a>
+         <nav id="mainNav">
+      <a href="#" class="logo">KITAB<span class="text_arb">كتاب</span></a>
+      <ul class="nav-links">
+        <li>
+          <a href="marketplace.html" id="nav_marketplace" class="active"
+            ><span class="material-icons">local_mall</span> Marketplace</a
+          >
         </li>
         <li>
-          <a href="codeHTML.html"><i class="bi bi-repeat"></i>Exchange</a>
+          <a href="messages.html" id="nav_messages"
+            ><span class="material-icons">chat_bubble</span> Messages</a
+          >
         </li>
         <li>
-          <a href="favorisContenantLivres.html"><i class="bi bi-heart"></i>Favoris</a>
+          <a href="codeHTML.html" id="nav_exchanges"
+            ><span class="material-icons">swap_horiz</span> Exchanges</a
+          >
         </li>
         <li>
-          <a href="reading-rooms.html"><i class="bi bi-book"></i>Reading Room</a>
+          <a
+            href="favorisContenantLivres.html"
+            id="nav_favorites"
+            ><span class="material-icons">favorite</span> Favorites</a
+          >
         </li>
-        <hr />
         <li>
-          <a href="profile.html"><i class="bi bi-person-circle"></i>Profile</a>
+          <a href="reading-rooms.html" id="nav_reading_rooms"
+            ><span class="material-icons">import_contacts</span> Reading
+            Rooms</a
+          >
         </li>
-        <li>
-          <a href=""><i class="bi bi-gear"></i>Settings</a>
-        </li>
-        <hr />
       </ul>
+
+      <div class="nav-right">
+        <a href="connect.html" id="nav_profile"
+          ><span class="material-icons">account_circle</span> Profile</a
+        >
+        <a href="logout.html" id="nav_logout"
+          ><span class="material-icons">logout</span>Log out</a
+        >
+
+        <div class="lang-switcher-nav">
+          <button class="lang-btn" id="btn-en">EN</button>
+          <button class="lang-btn" id="btn-fr">FR</button>
+          <button class="lang-btn" id="btn-ar">AR</button>
+        </div>
+      </div>
     </nav>
         <section id="create-account" class="position-relative end-0 min-vh-100 p-4 exchanges mt-5 pt-5" >
             <div class="position-relative mb-4">
